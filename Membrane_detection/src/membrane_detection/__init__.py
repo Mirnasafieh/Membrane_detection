dcb9002ee5aef6021ca912405ac6cc2ecc944777
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-13 13:11:03 +0300

    dd

--- Membrane_detection/src/membrane_detection/__init__.py
+++ Membrane_detection/src/membrane_detection/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.0"
 __title__ = "Membrabe_detection"
 __description__ = "Parse Stuff With Ease"
-__url__ = <GITHUB LINK>
+__url__ = "https://github.com/Mirnasafieh/Membrane_detection.git"
 __doc__ = __description__ + " <" + __url__ + ">"
 __author__ = "Mirna Safieh and Hila Ben-Moshe"
 __email__ = "mirna.safieh.91@gmail.com"
