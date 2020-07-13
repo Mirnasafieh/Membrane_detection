6cb1832aa28721c5c50d4eb4a8beafb3106587de
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-13 12:04:16 +0300

    changes

---
+++ Membrane_detection/src/membrane_detection/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__title__ = "Parse Stuff"
+__description__ = "Parse Stuff With Ease"
+__url__ = <GITHUB LINK>
+__doc__ = __description__ + " <" + __url__ + ">"
+__author__ = <YOUR NAME>
+__email__ = <YOUR EMAIL>
+__license__ = "MIT"
