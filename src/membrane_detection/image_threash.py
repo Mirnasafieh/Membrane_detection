a2f25dd0cbe5cd0734e7832beaaf9ca86b4c63fc
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-20 17:12:47 +0300

    changes

---
+++ src/membrane_detection/image_threash.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import skimage
+import skimage.io
+from skimage import data, util, measure
+import pandas as pd 
+import imagecodecs
+import tifffile 
+from skimage.color import rgb2gray
+
+def grayschale(img):
+    """converts the image into a grayscale"""
+    original = skimage.io.imread(img)
+    grayscale = rgb2gray(original)
+
+    return grayscale
+a=grayschale('e3 hol 1250 1500_z0_ch02.tif')
+print(a)
+
+def create_hotogram(img):
+    """This function creates a histogram for the image given, image should be inputed in a grayscale"""
+    hist, bins = skimage.exposure.histogram(img)
+    plt.plot(bins, hist, linewidth=1)
+    plt.xlabel('pixel value (a.u.)')
+    plt.ylabel('counts')
+    plt.show()
+b= create_hotogram(a)
+
+def image_thresh(img, thresh):
+    """ This function when given a picture and a threshold value, returns an image after thresholding"""
+    thresh_val = thresh
+    thresh_im = img < thresh_val
+    plt.imshow(thresh_im, cmap='gray')
+    plt.show()
+    return thresh_im
+
+c= image_thresh(a, 0.0095)
+print (c)
+
+def image_measurements(img):
+    total_area=img.size
+    stained_area=np.count_nonzero(img)
+    percent_area=stained_area/total_area
+    total_intensity=np.sum(img)
+    mean_intensity=np.mean(img)
+    Intigrated_optical_density=total_intensity*stained_area
+
