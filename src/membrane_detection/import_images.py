97609a4c312c9d9a5c4aed1f8becebe68f3cef5c
bmhila <bmhila@gmail.com>
2020-07-20 23:20:41 +0300

    addung images folder and import images

--- src/membrane_detection/import_images.py
+++ src/membrane_detection/import_images.py
@@ -1,9 +1,18 @@
-import pims
-import numpy
-from pims import ImageSequence
+from PIL import Image
+import pathlib
+import matplotlib.pyplot as plt 
 
+def import_images(foldername):
+  X_data = []
+  pathname = pathlib.Path(foldername)
+  for x in pathname.iterdir():
+    xname = pathlib.Path(x).name
+    if xname.endswith('01.tif'):
+      im = Image.open(x)
+      plt.imshow(im)
+      plt.show()
+      X_data.append(im)
+  print (X_data)
 
 if __name__ == "__main__":
-  new_images = ImageSequence('img-*.tiff')
-  a = 8
-  print (a)
+  import_images('images')
