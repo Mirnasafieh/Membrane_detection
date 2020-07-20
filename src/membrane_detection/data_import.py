12c1b62871815c862e2cc1ac3a7d08fc18b7db0d
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-20 10:14:31 +0300

    data imporr done

--- src/membrane_detection/data_import.py
+++ src/membrane_detection/data_import.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import pathlib
 
-df = pd.read_excel (r'D:\DannyM19\Desktop\Membrane detection\Mirna project data.xlsx')
-print (df)
+def data_import(filename):
+    """this function import an excel file with the data and returns a pandas dataframe"""
+        
+    p = pathlib.Path(filename)
+    df = pd.read_excel (p)
+    return df
+
+
