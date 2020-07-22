dde416debbe286f317bb63cc56583c2488eadf11
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-22 12:06:20 +0300

    changes

--- src/membrane_detection/canny.py
+++ src/membrane_detection/canny.py
@@ -14,10 +14,19 @@ from skimage import data, exposure, img_as_float
 from skimage.filters import unsharp_mask
 import matplotlib.pyplot as plt
 from cv2 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from skimage import data
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.color import label2rgb
+
 
 
 def plot_comparison(original, filtered, filter_name):
-
+    """plots two images together for comparison"""
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4), sharex=True,
                                    sharey=True)
     ax1.imshow(original, cmap=plt.cm.gray)
@@ -34,68 +43,70 @@ def grayschale(img):
 
     return grayscale
 
-im=grayschale('new_image_2.jpg')
-#plt.imshow(im, cmap='gray')
-#plt.show()
-
 def sharpen(img):
+    """sharpens the image"""
     result = unsharp_mask(img, radius=2, amount=2)
     return result
 
-im2= sharpen(im)
-#plt.imshow(im2, cmap='gray')
-#plt.show()
-
-plot_comparison(im, im2, "sharpen")
-
-def edge_detec(img):
-    edges = feature.canny(img, sigma=0.7)
+def edge_detec(img,sigma_val):
+    """edge detection using skimage canny"""
+    edges = feature.canny(img, sigma=sigma_val)
     return edges
 
-im3=edge_detec(im2)
-#plt.imshow(im3, cmap='gray')
-#plt.show()
-plot_comparison(im2, im3, "edge detect")
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
-from skimage import data
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-from skimage.color import label2rgb
 
 
-image = im3
 
-# apply threshold
-thresh = threshold_otsu(image)
-bw = closing(image > thresh, square(3))
+im=grayschale('new_image.tif')
+im_roi=grayschale('new_image_2.jpg')
 
-# remove artifacts connected to image border
-cleared = clear_border(bw)
+im2= sharpen(im)
+im2_roi= sharpen(im_roi)
 
-# label image regions
-label_image = label(cleared)
-# to make the background transparent, pass the value of `bg_label`,
-# and leave `bg_color` as `None` and `kind` as `overlay`
-image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+im3=edge_detec(im2,0.5)
+im3_roi=edge_detec(im2_roi,0.5)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.imshow(image_label_overlay)
 
-for region in regionprops(label_image):
-    # take regions with large enough areas
-    if region.area >= 100:
-        # draw rectangle around segmented coins
-        minr, minc, maxr, maxc = region.bbox
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                  fill=False, edgecolor='red', linewidth=2)
-        ax.add_patch(rect)
+plot_comparison(im2, im3, "edge detect segma 0.5 no roi")
 
-ax.set_axis_off()
-plt.tight_layout()
-#plt.show()
+plot_comparison(im2_roi, im3_roi, "edge detect segma 0.5 roi")
+plt.show()
+
+
+kernel = np.ones((5,5),np.uint8)
+erosion = cv2.erode(im3,kernel,iterations = 1)
+gradient = cv2.morphologyEx(im3, cv2.MORPH_GRADIENT, kernel)
+plot_comparison(im3,gradient , "gradient")
+plt.show()
+
+""" the following code recognized subpopulations in the picture, maybe we could use it to eliminate them and clean the picture"""
+# image = im3
+
+# # apply threshold
+# thresh = threshold_otsu(image)
+# bw = closing(image > thresh, square(3))
+
+# # remove artifacts connected to image border
+# cleared = clear_border(bw)
+
+# # label image regions
+# label_image = label(cleared)
+# # to make the background transparent, pass the value of `bg_label`,
+# # and leave `bg_color` as `None` and `kind` as `overlay`
+# image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+
+# fig, ax = plt.subplots(figsize=(10, 6))
+# ax.imshow(image_label_overlay)
+
+# for region in regionprops(label_image):
+#     # take regions with large enough areas
+#     if region.area >= 100:
+#         # draw rectangle around segmented coins
+#         minr, minc, maxr, maxc = region.bbox
+#         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+#                                   fill=False, edgecolor='red', linewidth=2)
+#         ax.add_patch(rect)
+
+# ax.set_axis_off()
+# plt.tight_layout()
+# plt.show()
 
