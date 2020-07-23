568b57efd2405713706cf232455dbcde6ed8aeb7
bmhila <bmhila@gmail.com>
2020-07-23 22:51:17 +0300

    statistics finished

--- src/membrane_detection/statistics.py
+++ src/membrane_detection/statistics.py
@@ -1,15 +1,11 @@
-## Import the packages
-import numpy as np
-from scipy import stats
-import pandas as pd
-from scipy.stats import ttest_ind
 import researchpy
-import csv
-import openpyxl
+import pandas as pd
+import pathlib
 from openpyxl import load_workbook
 
 
 def groups_IOD(results_file):
+    """Returns two groups of IOD parameter for receptor variable sorted by genotype"""
     sum_data = pd.read_excel(results_file)
     group1 = sum_data['IOD'].where((sum_data['cell genotype'] == 'e3') | (sum_data['cell genotype'] == 'E3')).dropna()
     group2 = sum_data['IOD'].where((sum_data['cell genotype'] == 'e4') | (sum_data['cell genotype'] == 'E4')).dropna()
@@ -17,6 +13,7 @@ def groups_IOD(results_file):
 
 
 def groups_colocalization(results_file, name_com):
+    """Returns two groups of M1 parametr for compartment parametr sorted by genotype"""
     sum_data = pd.read_excel(results_file)
     group1 = sum_data['M1'].where(((sum_data['cell genotype'] == 'e3') | (sum_data['cell genotype'] == 'E3'))
         & (sum_data['compartment'] == name_com)).dropna()
@@ -26,16 +23,28 @@ def groups_colocalization(results_file, name_com):
 
 
 def stat_groups(stat_file, group1, group2):
+    """Returns statistic analysis of two groups"""
     descriptive_table, result_table = researchpy.ttest(group1, group2)
     descriptive_table = descriptive_table.rename(index={0: 'ApoE3', 1: 'ApoE4', 2: 'ApoE3 + ApoE4'})
     return descriptive_table, result_table
 
 
 def export_stat(descriptive_table, result_table, name_var):
-    with pd.ExcelWriter("sum statistics.xlsx", engine='xlsxwriter') as writer:
-        descriptive_table.to_excel(writer, name_var + '_1')
-        result_table.to_excel(writer, name_var + '_2')
-        writer.save()
+    """Export data to excel file"""
+    sum_file = 'sum statistics.xlsx'
+    if pathlib.Path(sum_file).exists():
+        book = load_workbook(pathlib.Path(sum_file))
+        with pd.ExcelWriter(pathlib.Path(sum_file), engine='openpyxl') as writer:
+            writer.book = book
+            writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+            descriptive_table.to_excel(writer, sheet_name=name_var + '_1')
+            result_table.to_excel(writer, sheet_name=name_var + '_2') 
+            writer.save()
+    else:
+        with pd.ExcelWriter(sum_file, engine='xlsxwriter') as writer:
+            descriptive_table.to_excel(writer, sheet_name=name_var + '_1')
+            result_table.to_excel(writer, sheet_name=name_var + '_2')
+            writer.save()
 
 
 if __name__ == "__main__":
