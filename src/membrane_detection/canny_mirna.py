0821a539d9c26b22d1324eb1cbae49249ae8a099
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-24 13:16:54 +0300

    canny done

--- src/membrane_detection/canny_mirna.py
+++ src/membrane_detection/canny_mirna.py
@@ -1,34 +1,23 @@
-from skimage import data
-from skimage import color
-from skimage.filters import meijering, sato, frangi, hessian
+from skimage.filters import meijering, sato, frangi, hessian, threshold_otsu, rank, unsharp_mask
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, label2rgb
 import skimage
 import skimage.io 
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
-from skimage import feature
 from PIL import Image, ImageEnhance
-from skimage import data, exposure, img_as_float
-from skimage.filters import unsharp_mask
-import matplotlib.pyplot as plt
+from skimage import data, exposure, img_as_float, morphology, filters, feature, color
 from cv2 import cv2
 import matplotlib.patches as mpatches
-from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-from skimage.color import label2rgb
 from skimage.util import img_as_ubyte
-from skimage.morphology import erosion, dilation, opening, closing, white_tophat
-from skimage.morphology import black_tophat, skeletonize, convex_hull_image
-from skimage.morphology import disk
+from skimage.morphology import erosion, dilation, opening, closing, white_tophat, flood_fill, black_tophat, skeletonize, convex_hull_image, disk, closing, square
 from scipy.ndimage import gaussian_filter
 from scipy import misc
-import matplotlib.pyplot as plt
-from skimage import filters
 from tifffile import imsave
+from skimage.util.dtype import dtype_range
+
 
 def plot_comparison(original, filtered, filter_name):
     """plots two images together for comparison"""
@@ -61,7 +50,7 @@ def edge_detec(img,sigma_val):
 def close_img (img):
     phantom = img
     phantom[10:30, 200:210] = 0
-    selem = disk(6)
+    selem = disk(10)
     closed = closing(phantom, selem)
     return closed 
 
@@ -71,8 +60,8 @@ def open_img (img):
     return opened 
 
 def dialate_img (img):
-    selem = disk(3)
-    dialated=dilation(opened_edge, selem)
+    selem = disk(4)
+    dialated=dilation(img, selem)
     return dialated
 
 def gaussian_filter_img (img):
@@ -86,64 +75,52 @@ def threshold_otsu_img(img):
     thresh=image < val
     return thresh
 
+def bf_prep(img):
+    
 if __name__ == "__main__":
-    im_gray=grayschale('image-0.tif')
+    im_gray=grayschale('new_image_4.tif')
     im_sharp= sharpen(im_gray)
 
-    filtered=gaussian_filter(im_sharp, sigma=1)
-    plot_comparison(im_sharp, filtered, 'opened1')
+    """option 1"""
 
-    opened = open_img (filtered)
-    plot_comparison(filtered, opened, 'opened1')
+    #gray scale picture:
+    img = im_sharp
 
-    opened_edge=edge_detec(opened,0.5)
-    plot_comparison(opened, opened_edge, 'edgedetec2')
-
-    dialated=dialate_img (opened_edge)
-    plot_comparison(opened_edge, dialated, 'dialated3')
-
-    opened_edge_closed=close_img (dialated)
-    
-    plot_comparison(opened_edge, opened_edge_closed, 'closed4')
-
-    plot_comparison(im_sharp, opened_edge_closed, 'final')
-
+    # Equalization threshold:
+    p2, p98 = np.percentile(img, (2, 98))
+    a = exposure.rescale_intensity(im_gray, in_range=(p2, p98))
+    plot_comparison(im_sharp, a, 'equalize')
     plt.show()
-    
+
+    #Gaussian:
+    b=gaussian_filter(a, sigma=2.7)
+    plot_comparison(a, b, 'gaussian')
+    plt.show()
+
+    #Edge detection: 
+    d=edge_detec(b,1)
+    plot_comparison(b, d, 'edge')
+    plt.show()
+
+    #remove small objects:
+    e = morphology.remove_small_objects(d, 200, in_place=True, connectivity=50)
+    plot_comparison(d, e, 'remove small objects')
+    plt.show()
+
+    #close:
+    f=close_img (e)
+    plot_comparison(e, f, 'close')
+    plt.show()
+
+    #dialated:
+    g=dialate_img (f)
+    plot_comparison(img, g, 'dialated')
+    plt.show()
 
 
+    #remove small objects:
+    h = morphology.remove_small_objects(g, 1700, in_place=True, connectivity=200)
+    plot_comparison(img, h, 'remove small objects')
+    plt.show()
 
-
-
-    # """ the following code recognized subpopulations in the picture, maybe we could use it to eliminate them and clean the picture"""
-    # image = im3
-
-    # # apply threshold
-    # thresh = threshold_otsu(image)
-    # bw = closing(image > thresh, square(3))
-
-    # # remove artifacts connected to image border
-    # cleared = clear_border(bw)
-
-    # # label image regions
-    # label_image = label(cleared)
-    # # to make the background transparent, pass the value of `bg_label`,
-    # # and leave `bg_color` as `None` and `kind` as `overlay`
-    # image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
-
-    # fig, ax = plt.subplots(figsize=(10, 6))
-    # ax.imshow(image_label_overlay)
-
-    # for region in regionprops(label_image):
-    #     # take regions with large enough areas
-    #     if region.area >= 100:
-    #         # draw rectangle around segmented coins
-    #         minr, minc, maxr, maxc = region.bbox
-    #         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-    #                                 fill=False, edgecolor='red', linewidth=2)
-    #         ax.add_patch(rect)
-
-    # ax.set_axis_off()
-    # plt.tight_layout()
-    # plt.show()
 
