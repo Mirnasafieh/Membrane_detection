290864603051c3fc06ad1685f587ac3382986f5f
mirnasafieh <mirna.safieh.91@gmail.com>
2020-07-25 19:10:20 +0300

    data merge fixed

--- src/membrane_detection/data_handeling.py
+++ src/membrane_detection/data_handeling.py
@@ -10,9 +10,16 @@ def data_import(filename):
     df = pd.read_excel(p)
     return df
 
-def data_merge(df1,df2):
+def data_merge(df1,df2,N):
     """This function merges between two dataframes - the existing one and the output dataframe according to cell genotype, N, and cell number"""
-    result = pd.merge(df1, df2,  how='left', left_on=['cell genotype','N','Cell number'], right_on = ['cell genotype','N','Cell number'])
+
+    if any(df1.N == N):
+
+        result = pd.merge(df1, df2,  how='left', left_on=['cell genotype','N','Cell number'], right_on = ['cell genotype','N','Cell number'])
+    else:
+  
+        result = pd.concat([df1, df2], ignore_index=True, sort=False)
+
     return result
 
 def export_to_excel(df, name):
@@ -24,9 +31,8 @@ def export_tif(img, name):
     imsave(name, img)
 
 if __name__ == "__main__":
-    df1=data_import('Mirna project data.xlsx')
-    df2=data_import('test for merge.xlsx')
-    df3=data_merge(df1,df2)
+    df1=data_import('ApoER2 colocalization.xlsx')
+    df2=data_import('try merge.xlsx')
+    df3=data_merge(df1,df2,2)
     export_to_excel(df3, "testing export.xlsx")
 
-    
